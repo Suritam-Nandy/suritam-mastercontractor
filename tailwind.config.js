60c4f40d91bf48ad43f69f09aba7d9900ef48cb4
Suritam-Nandy <13suritsuritam@gmail.com>
2021-05-02 19:13:32 +0530

    💄

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yl: "#ffc107",
+        offWhite: "#C4C4C4",
+      },
+    },
   },
   variants: {
     extend: {},
